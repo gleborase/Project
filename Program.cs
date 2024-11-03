e4f0369b656d031b8a08b789e754f2d577ee2715
gleborase <krasnopolsci@gmail.com>
2024-11-03 18:13:40 +0200

    add doc

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Task 1
+         // Console.Write("a=");
+         // int a = int.Parse(Console.ReadLine());
+        //  Console.WriteLine("b");
+         // int b = Convert.ToInt32(Console.ReadLine());
+        //  Console.WriteLine($"{a}+{b}={a+b}");
+         // Console.Read();
+
+            //Task 2
+         // Console.Write("c=");
+         // int c = int.Parse(Console.ReadLine());
+          //Console.WriteLine("d");
+         // int d = Convert.ToInt32(Console.ReadLine());
+         // Console.WriteLine($"{c}+{d}={d}+{c}");
+         // Console.Read();
+        }
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// Task 1
+         //   Console.Write("a=");
+         //   int a = int. Parse(Console.ReadLine());
+         //  Console.Write("b=");
+         //  int b = int. Parse(Console.ReadLine());
+         //  Console.WriteLine($"{a}+{b}={a + b}");
+
+            //TAsk 2 
+            //   Console.Write("a=");
+            //  int a = int.Parse(Console.ReadLine());
+            //   Console.Write("b=");
+            //   int b = int.Parse(Console.ReadLine());
+            //   Console.Write("c=");
+            //   int c = int. Parse(Console.ReadLine());
+            //   Console.WriteLine($"{a}+{b}+{c}={a+b+c}");
+
+            //Task 3
+            //   Console.Write("side1=");
+            //    int side1 = int.Parse(Console.ReadLine());
+            //  Console.Write("side2=");
+            //   int side2 = int.Parse(Console.ReadLine());
+            //   Console.WriteLine($"perimert={2*(side1+side2)}");
+
+           
+            Console.Read();
+
+        }
+    }
+}
